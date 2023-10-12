6002f4f544e11f584731817dab649d275b4ace5c
DubanR <duban212002@gmail.com>
2023-10-12 12:27:14 -0500

    feat: :sparkles: implement interfaces
    Create intefaces for entities
    
    @duvanrg

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -5,10 +5,14 @@ namespace Core.Interfaces
 {
     public interface IGenericRepository<T> where T : BaseEntity
     {
-        Task<T> getByIdAsync(int id);
+        Task<T> GetByIdAsync(int id);
         Task<IEnumerable<T>> GetAllAsync();
         IEnumerable<T> Find(Expression<Func<T, bool>> expression);
-        Task<(int totalRegistros, IEnumerable<T> registros)> GetAllAsync(int pageIndex, int pageSize, string Search);
-        void 
+        Task<(int totalRegistros, IEnumerable<T> registros)> GetAllAsync(int pageIndex, int pageSize, String Search);
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        void Update(T entity);
     }
 }
---
+++ Core/Interfaces/IRolVsMaestro.cs
@@ -0,0 +1,9 @@
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IRolVsMaestro : IGenericRepository<RolVsMaestro>
+    {
+         
+    }
+}
