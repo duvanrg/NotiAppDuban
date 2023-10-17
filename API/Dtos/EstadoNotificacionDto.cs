8bb9c4ef7a270e6d252588bd03bf2b769b0a45e0
DubanR <duban212002@gmail.com>
2023-10-17 06:31:09 -0500

    feat: Implemento mapping
    Implemento entities and dtos
    
    @duvanrg

---
+++ API/Dtos/EstadoNotificacionDto.cs
@@ -0,0 +1,9 @@
+namespace API.Dtos
+{
+    public class EstadoNotificacionDto
+    {
+        public string NombreEstado { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+    }
+}
---
+++ API/Profiles/MappingProfiles.cs
@@ -0,0 +1,28 @@
+using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+
+namespace ApiAnimals.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Auditoria, AuditoriaDto>().ReverseMap();
+            CreateMap<BlockChain, BlockChainDto>().ReverseMap();
+            CreateMap<EstadoNotificacion, EstadoNotificacionDto>().ReverseMap();
+            CreateMap<Formatos, FormatosDto>().ReverseMap();
+            CreateMap<GenericosVsSubmodulos, GenericosVsSubmodulosDto>().ReverseMap();
+            CreateMap<HiloRespuestaNotificacion, HilosRespuestaNotificacionDto>().ReverseMap();
+            CreateMap<MaestrosVsSubmodulos, MaestrosVsSubmodulosDto>().ReverseMap();
+            CreateMap<ModuloNotificaciones, ModuloNotificacionesDto>().ReverseMap();
+            CreateMap<PermisosGenericos, PermisosGenericosDto>().ReverseMap();
+            CreateMap<Radicados, RadicadosDto>().ReverseMap();
+            CreateMap<Rol, RolDto>().ReverseMap();
+            CreateMap<RolVsMaestro, RolVsMaestroDto>().ReverseMap();
+            CreateMap<Submodulos, SubmodulosDto>().ReverseMap();
+            CreateMap<TipoNotificacion, TipoNotificacionDto>().ReverseMap();
+            CreateMap<TipoRequerimiento, TipoRequerimientoDto>().ReverseMap();
+        }
+    }
+}
